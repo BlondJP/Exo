03fd466dfa9661b1d29b36c9c32c98257df04277
Jean-Philippe BLOND <jpb.blond@gmail.com>
2017-01-11 14:17:53 +0100

    Update Database.php

--- Database.php
+++ Database.php
@@ -3,27 +3,18 @@
 class Database 
 {
 	
-	private static $database; //si on veut utiliser une variable dans une fonction static il faut que celle la soit aussi static
+	private static $database;
 
-	public static function connect() //ici nous avons une fonction static qui pourra s'appeler sans l'instantiation d'une class
+	public static function connect()
 	{
-		try
-		{
-			self::$database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
-			echo 'database ouvert';
-			return self::$database; //l'utilisation d'une variable static s'utilise avec self::nomAttribut
- 		}
-		catch(Exception $e)
-		{
-		        die('Erreur : '.$e->getMessage());
-		}
+		self::$database = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', '');
+		return self::$database;
 	}
 
 
 	public static function disconnect()
 	{
-		self::$database = NULL; //je remet database a NULL pour ne pas pourrir ma base avec d'autre elements
-		echo 'database fermé';
+		self::$database = NULL;
 	}
 }
 
